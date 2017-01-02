df4b8c06092db45a5b7b21b0c6d949ee4e23f2b1
Roman Heinrich <roman.heinrich@gmail.com>
2017-01-02 12:23:04 +0100

    exclude humanize from code coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,13 @@
 require 'rubygems'
 require 'bundler/setup'
 
+require "byebug"
 require 'simplecov'
 SimpleCov.start do
   add_filter "/spec/"
   add_filter "/.direnv/"
   add_filter "/core_extensions/class_attribute" # copy from ActiveSupport
+  add_filter "/core_extensions/humanize" # copy from ActiveSupport
   add_filter "/attr_validator/concern" # copy from ActiveSupport
 end
 if ENV['CI']=='true'
