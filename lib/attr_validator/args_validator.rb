e497d790bff6237eaf876be7e07fe32cee18bda2
Roman Heinrich <roman.heinrich@gmail.com>
2017-01-02 14:12:53 +0100

    refactor args_validator to increase codeclimate score

--- lib/attr_validator/args_validator.rb
+++ lib/attr_validator/args_validator.rb
@@ -8,81 +8,63 @@ module AttrValidator::ArgsValidator
     # @param obj some object
     # @param obj_name object's name, used to clarify error causer in exception
     def is_symbol!(obj, obj_name)
-      unless obj.is_a?(Symbol)
-        raise ArgError, "#{obj_name} should be a Symbol"
-      end
+      allow!(obj, [Symbol], "#{obj_name} should be a Symbol")
     end
 
     # Checks that specifid +obj+ is a boolean
     # @param obj some object
     # @param obj_name object's name, used to clarify error causer in exception
     def is_boolean!(obj, obj_name)
-      if !obj.is_a?(TrueClass) && !obj.is_a?(FalseClass)
-        raise ArgError, "#{obj_name} should be a Boolean"
-      end
+      allow!(obj, [TrueClass, FalseClass], "#{obj_name} should be a Boolean")
     end
 
     # Checks that specifid +obj+ is an integer
     # @param obj some object
     # @param obj_name object's name, used to clarify error causer in exception
     def is_integer!(obj, obj_name)
-      unless obj.is_a?(Integer)
-        raise ArgError, "#{obj_name} should be an Integer"
-      end
+      allow!(obj, [Integer], "#{obj_name} should be an Integer")
     end
 
     # Checks that specifid +obj+ is an Array
     # @param obj some object
     # @param obj_name object's name, used to clarify error causer in exception
     def is_array!(obj, obj_name)
-      unless obj.is_a?(Array)
-        raise ArgError, "#{obj_name} should be an Array"
-      end
+      allow!(obj, [Array], "#{obj_name} should be an Array")
     end
 
     # Checks that specifid +obj+ is a Hash
     # @param obj some object
     # @param obj_name object's name, used to clarify error causer in exception
     def is_hash!(obj, obj_name)
-      unless obj.is_a?(Hash)
-        raise ArgError, "#{obj_name} should be a Hash"
-      end
+      allow!(obj, [Hash], "#{obj_name} should be a Hash")
     end
 
     # Checks that specifid +obj+ is an integer or float
     # @param obj some object
     # @param obj_name object's name, used to clarify error causer in exception
     def is_integer_or_float!(obj, obj_name)
-      if !obj.is_a?(Integer) && !obj.is_a?(Float)
-        raise ArgError, "#{obj_name} should be an Integer or Float"
-      end
+      allow!(obj, [Integer, Float], "#{obj_name} should be an Integer or Float")
     end
 
     # Checks that specifid +obj+ is a string or regexp
     # @param obj some object
     # @param obj_name object's name, used to clarify error causer in exception
     def is_string_or_regexp!(obj, obj_name)
-      if !obj.is_a?(String) && !obj.is_a?(Regexp)
-        raise ArgError, "#{obj_name} should be a String or Regexp"
-      end
+      allow!(obj, [String, Regexp], "#{obj_name} should be a String or Regexp")
     end
 
     # Checks that specifid +obj+ is a symbol or class
     # @param obj some object
     # @param obj_name object's name, used to clarify error causer in exception
     def is_class_or_symbol!(obj, obj_name)
-      if !obj.is_a?(Symbol) && !obj.is_a?(Class)
-        raise ArgError, "#{obj_name} should be a Symbol or Class"
-      end
+      allow!(obj, [Symbol, Class], "#{obj_name} should be a Symbol or Class")
     end
 
     # Checks that specifid +obj+ is a symbol or block
     # @param obj some object
     # @param obj_name object's name, used to clarify error causer in exception
     def is_symbol_or_block!(obj, obj_name)
-      if !obj.is_a?(Symbol) && !obj.is_a?(Proc)
-        raise ArgError, "#{obj_name} should be a Symbol or Proc"
-      end
+      allow!(obj, [Symbol, Proc], "#{obj_name} should be a Symbol or Proc")
     end
 
     # Checks that specifid +hash+ has a specified +key+
@@ -113,6 +95,13 @@ module AttrValidator::ArgsValidator
       unless block
         raise ArgError, "Block should be given"
       end
+    end
+
+    private
+
+    def allow!(obj, klasses, msg)
+      return if klasses.any?{|klass| obj.is_a?(klass)}
+      raise ArgError, msg
     end
   end
 end
