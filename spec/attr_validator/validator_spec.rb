f5701c0f9f1ee6b261be7af688c8804146fb4600
Roman Heinrich <roman.heinrich@gmail.com>
2017-01-02 10:25:00 +0100

    some code coverage increasement

--- spec/attr_validator/validator_spec.rb
+++ spec/attr_validator/validator_spec.rb
@@ -3,16 +3,28 @@ require 'spec_helper'
 describe AttrValidator::Validator do
   describe "#validate" do
     class Contact
-      attr_accessor :first_name, :last_name, :position, :age, :type, :email, :color, :status, :stage, :description, :companies
+      attr_accessor :first_name, :last_name, :position, :age, :type, :email, :color, :status, :stage, :description, :companies, :address
     end
+
     class Company
       attr_accessor :name
+    end
+
+    class Address
+      attr_accessor :city, :street
     end
 
     class CompanyValidator
       include AttrValidator::Validator
 
       validates :name, presence: true, length: { min: 3, max: 9 }
+    end
+
+    class AddressValidator
+      include AttrValidator::Validator
+
+      validates :city, presence: true, length: { min: 3, max: 33 }
+      validates :street, presence: true, length: { min: 3, max: 33 }
     end
 
     class ContactValidator
@@ -29,6 +41,10 @@ describe AttrValidator::Validator do
       validates :stage,      exclusion: { in: [:wrong, :bad] }
 
       validate_associated :companies, validator: CompanyValidator
+
+      validates :address, presence: true
+      validate_associated :address, validator: AddressValidator
+
 
       validate :check_description
 
@@ -58,6 +74,9 @@ describe AttrValidator::Validator do
       company2.name = 'ICL'
 
       contact.companies = [company1, company2]
+
+      address = Address.new.tap do |a| a.city = "New York" ; a.street = "Wall Street" end
+      contact.address = address
 
       errors = ContactValidator.new.validate(contact)
       errors.should be_empty
@@ -96,9 +115,48 @@ describe AttrValidator::Validator do
         companies_errors: [
           { name: ["can not be more than 9"] },
           { name: ["can not be less than 3"] },
-        ]
+        ],
+        address: ["can not be blank"],
       }
     end
   end
 
+  context "some error cases" do
+    context "validate" do
+      it "raises with improper :validate definition" do
+        expect{
+          class ValidateWithoutArgsValidator
+            include AttrValidator::Validator
+            validate
+          end
+        }.to raise_error(ArgumentError, "method name or block should be given for validate")
+      end
+    end
+
+    it "raises with non-existing validators" do
+      expect {
+        validator_class = Class.new do
+          include AttrValidator::Validator
+          validates :last_name,  lengths: { equal_to: 5 }
+        end
+      }.to raise_error(AttrValidator::Errors::MissingValidatorError, "Validator with name 'lengths' doesn't exist")
+    end
+
+    it "allows procs as validators" do
+      validator_class = Class.new do
+        include AttrValidator::Validator
+
+        validate :shtick do |entity, errors|
+          errors.set(:shtick, AttrValidator::I18n.t('errors.can_not_be_nil'))
+        end
+      end
+
+      klass = Class.new do
+        attr_accessor :shtick
+      end
+
+      expect(validator_class.new.validate(klass.new)).to eq({:shtick=>"can not be nil"})
+      expect(validator_class.new.validate({})).to eq({:shtick=>"can not be nil"})
+    end
+  end
 end
